035b117105e259008d3a63e325e0f2f89f582db8
Jeff Bothe <jeffrymbothe@gmail.com>
2017-08-26 16:18:31 -0500

    attempt storing audio as arraybuffers

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,21 @@
 'use strict';
 
 jQuery(($) => {
-  const model = {
-    808: {
-      path: 'assets/808/',
-      files: ['clap', 'crash', 'hat', 'kick', 'snare', 'tom', 'bell', 'click'],
-    },
+  var audio_context = new AudioContext();
 
-    electro: {
-      path: 'assets/electro/',
-      files: ['clap', 'crash', 'hat', 'kick', 'snare', 'tom', 'laser', 'shake'],
+  const model = {
+    kits: {
+      808: {
+        path: 'assets/808/',
+        files: ['clap', 'crash', 'hat', 'kick', 'snare', 'tom', 'bell', 'click'],
+        buffers: [],
+      },
+
+      electro: {
+        path: 'assets/electro/',
+        files: ['clap', 'crash', 'hat', 'kick', 'snare', 'tom', 'laser', 'shake'],
+        buffers: [],
+      },
     },
 
     activeKit: 'electro',
@@ -22,24 +28,26 @@ jQuery(($) => {
     },
 
     toggleActiveKit: function toggleActiveKit() {
-      this.activeKit = this.activeKit == '808' ? 'electro' : '808';
+      this.activeKit = this.activeKit === '808' ? 'electro' : '808';
     },
 
-    triggerSound: function triggerSound(drum) {
-      const sound = new Audio(`${model[model.activeKit].path}${drum}.wav`);
-      sound.play();
+    triggerSound: function playSound(buffer) {
+      const source = audio_context.createBufferSource();
+      source.buffer = buffer;
+      source.connect(audio_context.destination);
+      source.start(0);
     },
 
     runSequence: function runSequence() {
       for (let i = 1; i < 65; i++) {
-        let step = i % 16;
-        setTimeout(function () {
-          $('.sequence-row').each(function (index) {
+        const step = i % 16;
+        setTimeout(() => {
+          $('.sequence-row').each(function iterate(index) {
             if ($(`:nth-child(${step})`, this).hasClass('active-block')) {
-              model.triggerSound($(`#drum${index + 1}`).text());
+              model.triggerSound(model.kits[model.activeKit].buffers[index]);
             }
           });
-        }, i * 100);
+        }, i * 200);
       }
     },
   };
@@ -50,6 +58,8 @@ jQuery(($) => {
 
   const controller = {
     initialize: function initialize() {
+      window.AudioContext = window.AudioContext || window.webkitAudioContext;
+      this.loadSounds();
       this.setupKit();
       this.kitClickListener();
       this.drumClickListener();
@@ -57,17 +67,31 @@ jQuery(($) => {
       this.playClickListener();
     },
 
-    setupKit: function setupKit() {
-      model.toggleActiveKit();
-      $('.kit').text(`${model.activeKit} kit`);
-      $('.drum').text((index) => {
-        return model[model.activeKit].files[index];
+    loadSounds: function loadSounds() {
+      Object.keys(model.kits).forEach((object) => {
+        model.kits[object].files.forEach((item, index) => {
+          const request = new XMLHttpRequest();
+          const audioUrl = `${model.kits[object].path}${item}.wav`;
+
+          request.open('GET', audioUrl, true);
+          request.responseType = 'arraybuffer';
+
+          request.onload = function onload() {
+            audio_context.decodeAudioData(request.response, (buffer) => {
+              model.kits[object].buffers[model.kits[object].files.indexOf(item)] = buffer;
+            });
+          };
+          request.send();
+        });
       });
     },
 
-    triggerSound: function triggerSound(e) {
-      const sound = new Audio(`${model[model.activeKit].path}${e.target.textContent}.wav`);
-      sound.play();
+    setupKit: function setupKit() {
+      model.toggleActiveKit();
+      $('.kit').text(`${model.activeKit} kit`);
+      $('.drum').text(index =>
+        model.kits[model.activeKit].files[index],
+      );
     },
 
     toggleActiveSequenceBlock: function toggleActiveSequenceBlock(e) {
