041dd67849a16b4d9734b2b730c497eb233a5760
Jeff Bothe <jeffrymbothe@gmail.com>
2017-08-26 10:41:57 -0500

    prelim spaghetti sequence function

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 
 jQuery(($) => {
   const model = {
-    '808': {
+    808: {
       path: 'assets/808/',
       files: ['clap', 'crash', 'hat', 'kick', 'snare', 'tom', 'bell', 'click'],
     },
@@ -12,10 +12,35 @@ jQuery(($) => {
       files: ['clap', 'crash', 'hat', 'kick', 'snare', 'tom', 'laser', 'shake'],
     },
 
-    activeKit: '808',
+    activeKit: 'electro',
+
+    play: false,
+
+    togglePlay: function togglePlay() {
+      this.play = this.play === false;
+      model.runSequence();
+    },
 
     toggleActiveKit: function toggleActiveKit() {
       this.activeKit = this.activeKit == '808' ? 'electro' : '808';
+    },
+
+    triggerSound: function triggerSound(drum) {
+      const sound = new Audio(`${model[model.activeKit].path}${drum}.wav`);
+      sound.play();
+    },
+
+    runSequence: function runSequence() {
+      for (let i = 1; i < 65; i++) {
+        let step = i % 16;
+        setTimeout(function () {
+          $('.sequence-row').each(function (index) {
+            if ($(`:nth-child(${step})`, this).hasClass('active-block')) {
+              model.triggerSound($(`#drum${index + 1}`).text());
+            }
+          });
+        }, i * 100);
+      }
     },
   };
 
@@ -25,27 +50,32 @@ jQuery(($) => {
 
   const controller = {
     initialize: function initialize() {
-      this.setupKit()
-      this.kitClickListener()
+      this.setupKit();
+      this.kitClickListener();
       this.drumClickListener();
-      this.sequenceBlockClickListener()
+      this.sequenceBlockClickListener();
+      this.playClickListener();
     },
 
     setupKit: function setupKit() {
       model.toggleActiveKit();
-      $('.kit')[0].textContent = `${model.activeKit} kit`;
-      $('.drum').each((index, item) => {
-        item.textContent = model[model.activeKit].files[index];
+      $('.kit').text(`${model.activeKit} kit`);
+      $('.drum').text((index) => {
+        return model[model.activeKit].files[index];
       });
     },
 
     triggerSound: function triggerSound(e) {
-      const sound = new Audio(`assets/kit-808/${e.target.textContent}.wav`);
+      const sound = new Audio(`${model[model.activeKit].path}${e.target.textContent}.wav`);
       sound.play();
     },
 
     toggleActiveSequenceBlock: function toggleActiveSequenceBlock(e) {
       $(e.target).toggleClass('active-block');
+    },
+
+    playClickListener: function playClickListener() {
+      $('.play').on('click', model.togglePlay.bind(model));
     },
 
     kitClickListener: function kitClickListener() {
