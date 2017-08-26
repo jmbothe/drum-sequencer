dda7d7ecf6ac13cd6b23dcb21800f19f1cc729bd
Jeff Bothe <jeffrymbothe@gmail.com>
2017-08-26 09:33:52 -0500

    js in development 1

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,21 @@
 
 jQuery(($) => {
   const model = {
+    '808': {
+      path: 'assets/808/',
+      files: ['clap', 'crash', 'hat', 'kick', 'snare', 'tom', 'bell', 'click'],
+    },
 
+    electro: {
+      path: 'assets/electro/',
+      files: ['clap', 'crash', 'hat', 'kick', 'snare', 'tom', 'laser', 'shake'],
+    },
+
+    activeKit: '808',
+
+    toggleActiveKit: function toggleActiveKit() {
+      this.activeKit = this.activeKit == '808' ? 'electro' : '808';
+    },
   };
 
   const view = {
@@ -10,14 +24,42 @@ jQuery(($) => {
   };
 
   const controller = {
-    playDrum: function playDrum(e) {
-      const sound = $(`audio[src*='${e.target.textContent}']`)[0];
+    initialize: function initialize() {
+      this.setupKit()
+      this.kitClickListener()
+      this.drumClickListener();
+      this.sequenceBlockClickListener()
+    },
+
+    setupKit: function setupKit() {
+      model.toggleActiveKit();
+      $('.kit')[0].textContent = `${model.activeKit} kit`;
+      $('.drum').each((index, item) => {
+        item.textContent = model[model.activeKit].files[index];
+      });
+    },
+
+    triggerSound: function triggerSound(e) {
+      const sound = new Audio(`assets/kit-808/${e.target.textContent}.wav`);
       sound.play();
     },
+
+    toggleActiveSequenceBlock: function toggleActiveSequenceBlock(e) {
+      $(e.target).toggleClass('active-block');
+    },
+
+    kitClickListener: function kitClickListener() {
+      $('.kit').on('click', this.setupKit);
+    },
+
+    sequenceBlockClickListener: function sequenceBLockClickListener() {
+      $('.sequence-block').on('click', this.toggleActiveSequenceBlock);
+    },
+
     drumClickListener: function drumClickListener() {
-      $('.drums-bar').on('click', this.playDrum);
+      $('.drums-bar').on('click', this.triggerSound);
     },
   };
 
-  controller.drumClickListener();
+  controller.initialize();
 });
